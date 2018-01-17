bd73bdba2f28ce73724f5af96d54cd37e89f96c3
Joseph Burdick <josephdburdick@gmail.com>
2018-01-17 14:51:57 -0500

    Boots Wordpress + WP-cli.

--- install.sh
+++ install.sh
@@ -2,77 +2,39 @@
 
 # SETTINGS
 DOCKER_COMPOSE_FILE="docker-compose.yml"
-DB_IMPORT="/var/www/db/wordpress-export.xml"
-MYSQL_ROOT_PASSWORD="50UP3RH4X!"
 
-# IMAGES
-WORDPRESS_IMAGE="wordpress:4.9.1"
-MYSQL_IMAGE="mysql:5.7"
-WPCLI_IMAGE="tatemz/wp-cli"
-
-# ############################
-# DO NOT EDIT BEYOND THIS LINE
-
-# Argument -i for install overwrites defined location
-# of imported backup/database
-if [ -z $i ]; then
-    DB_IMPORT=$i
-fi
-
+mkdir -p wp-site && cd wp-site
 touch "${DOCKER_COMPOSE_FILE}"
+
 cat > "${DOCKER_COMPOSE_FILE}" <<EOL
 version: '2'
 services:
-  wordpress:
-    depends_on:
-      - db
-    image: $WORDPRESS_IMAGE
-    restart: always
-    volumes:
-      - ./wp-content:/var/www/html/wp-content
-      - ./db:/var/www/db
-    environment:
-      WORDPRESS_DB_HOST: db:3306
-      WORDPRESS_DB_PASSWORD: password
+  my-wpdb:
+    image: mariadb
     ports:
-      - 80:80 # Expose http and https
-      - 443:443
-    networks:
-      - back
-  db:
-    image: $MYSQL_IMAGE
-    restart: always
-    volumes:
-       - db_data:/var/lib/mysql
+      - "8081:3306"
     environment:
-      MYSQL_ROOT_PASSWORD: $MYSQL_ROOT_PASSWORD
-    networks:
-      - back
-  phpmyadmin:
-    depends_on:
-      - db
-    image: phpmyadmin/phpmyadmin
-    restart: always
-    ports:
-      - 8080:80
-    environment:
-      PMA_HOST: db
-      MYSQL_ROOT_PASSWORD: $MYSQL_ROOT_PASSWORD
-    networks:
-      - back
-  wpcli:
-    image: $WPCLI_IMAGE
-    volumes_from:
-      - wordpress
-    links:
-      - db:mysql
-    entrypoint: wp
-    command: "import $DB_IMPORT --authors=create --debug"
-networks:
-  back:
-volumes:
-  db_data:
+      MYSQL_ROOT_PASSWORD: root
 
+  my-wp:
+    image: wordpress
+    volumes:
+      - ./:/var/www/html
+    ports:
+      - "8080:80"
+    links:
+      - my-wpdb:mysql
+    environment:
+      WORDPRESS_DB_PASSWORD: root
+
+  my-wpcli:
+    image: tatemz/wp-cli
+    volumes_from:
+      - my-wp
+    links:
+      - my-wpdb:mysql
+    entrypoint: wp
+    command: "--info"
 EOL
 
 docker-compose up -d
